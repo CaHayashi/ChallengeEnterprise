e4d27b5b2ea6ab2e2f9bde5d31f89031032cdc7c
CaHayashi <dududuhay@gmail.com>
2021-09-12 12:13:39 -0300

    <Entregavel Enterprise>

---
+++ Challenge1/Controllers/ProdutoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Challenge1.Models;
+
+
+namespace Challenge1.Controllers
+{
+    class ProdutoController : Controller
+    {
+
+        // CRIANDO UMA LISTA QUE TERÁ ARMAZENAMENTO EM MEMÓRIA, SIMULANDO O BANCO DE DADOS 
+        private static IList<Produto> _estoque = new List<Produto>();
+
+        private static int index;
+
+
+
+
+    }
+}
---
+++ Challenge1/Models/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenge1.Models
+{
+    public class Produto
+    {
+
+        private int Id { get; set;}
+        private string Nome { get; set; }
+        private string Peso { get; set; }
+        private int Quantidade { get; set; }
+        private DateTime DataValidade { get; set; }
+        private DateTime DataCompra { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Nome: {Nome}, Peso: {Peso}, Quantidade: {Quantidade}, Data de Validade: {DataValidade}, Data da Compra: {DataCompra}";
+        }
+
+        public Produto (int id, string nome, string peso, int quantidade, DateTime dataValidade, DateTime dataCompra)
+        {
+            Id = id;
+            Nome = nome;
+            Peso = peso;
+            Quantidade = quantidade;
+            DataValidade = dataValidade;
+            DataCompra = dataCompra;
+        }
+    }
+}
